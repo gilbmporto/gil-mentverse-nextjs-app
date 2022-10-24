54110351ed1448e81ec181280d2468d4b06f9b3e
GilBMP <gilbmp@gmail.com>
2022-10-24 20:25:08 -0300

    Modified signin page so it would have the correct logo and sign in message

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -4,21 +4,22 @@ import Header from '../../components/Header';
 function signIn({ providers }) {
   return (
     <>
-    <Header />
+      <Header />
       <div className='flex flex-col items-center justify-center min-h-screen py-2 px-14 -mt-10 text-center'>
-        <img 
-        className='w-80'
-        src='https://i.ibb.co/fvvDhQf/Insta-Bros-Logo-Correct-Width-2-0.png'
-        alt='Instagram Logo' 
+        <img
+          className='w-80'
+          src='https://i.ibb.co/yQ5jwPG/Insta-Bros-Logo-Correct-Width-2-0-1.png'
+          alt='Instagram Logo'
         />
-        <p className='font-xs italic'>Welcome to InstaBros, an instagram created just for Broderagens</p>
+        <p className='font-xs italic'>Welcome to PicPost, where everyone may post (almost) anything!</p>
+        <p className='font-xs italic'>(This was made for educational purposes only)</p>
 
         <div className='mt-36 '>
           {providers && Object.values(providers).map((provider) => (
             <div key={provider.name}>
-              <button 
-              className='p-3 bg-blue-500 rounded-lg text-white' 
-              onClick={() => SignIntoProvider(provider.id, { callbackUrl: '/' })}
+              <button
+                className='p-3 bg-blue-500 rounded-lg text-white'
+                onClick={() => SignIntoProvider(provider.id, { callbackUrl: '/' })}
               >
                 Sign in with {provider.name}
               </button>
